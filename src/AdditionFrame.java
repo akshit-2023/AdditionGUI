3af3c806f0f72676a278343054ef0726834cba82
Akshit <akshitv2004@gmail.com>
2024-09-21 15:38:00 +0530

    Addition Implemented

--- src/AdditionFrame.java
+++ src/AdditionFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AdditionFrame extends JFrame {//extended JFrame so that when obj of AdditionFrame is made it will be a frame as it extends JFrame, or we can create an object of JFrame in my class
     private JPanel panel;
@@ -16,13 +18,23 @@ public class AdditionFrame extends JFrame {//extended JFrame so that when obj of
         l2.setBounds(100,110,120,20);
         t2.setBounds(205,110,100,20);
         b1.setBounds(150,150,100,20);
-        l3.setBounds(120,180,100,20);
+        l3.setBounds(120,180,180,20);
         panel.add(l1);//To add label on panel
         panel.add(t1);
         panel.add(l2);
         panel.add(t2);
         panel.add(b1);
         panel.add(l3);
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int a=Integer.parseInt(t1.getText());
+                int b=Integer.parseInt((t2.getText()));
+                int c=a+b;
+                l3.setText("Result of Addition is: "+c);
+            }
+        });
 
 
 
@@ -36,4 +48,6 @@ public class AdditionFrame extends JFrame {//extended JFrame so that when obj of
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//To make the cross on frame to terminate the program,the argument is an integer value
 
     }
+
+
 }
